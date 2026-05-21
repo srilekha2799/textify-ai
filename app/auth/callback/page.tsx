2147cf0ad195ce2cd5210cb0332ebc661ecc1c0d
srilekha2799 <srilekhagondi2007@gmail.com>
2026-05-21 16:04:46 +0530

    testing callback route

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,55 +1,8 @@
-"use client";
-
-import { useEffect } from "react";
-
-import { useRouter } from "next/navigation";
-
-import { supabase } from "@/lib/supabase";
-
 export default function AuthCallback() {
 
-  const router = useRouter();
-
-  useEffect(() => {
-
-    const handleLogin =
-      async () => {
-
-        const {
-          data,
-        } =
-          await supabase.auth.getSession();
-
-        console.log(data);
-
-        if (data.session) {
-
-          router.replace(
-            "/homepage"
-          );
-
-        } else {
-
-          router.replace(
-            "/login"
-          );
-        }
-      };
-
-    handleLogin();
-
-  }, [router]);
-
   return (
-
-    <main className="flex min-h-screen items-center justify-center">
-
-      <h1 className="text-2xl font-bold">
-
-        Signing in...
-
-      </h1>
-
-    </main>
+    <h1>
+      Callback Working
+    </h1>
   );
 }
