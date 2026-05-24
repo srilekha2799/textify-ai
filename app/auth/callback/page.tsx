b3a978a1d33487317ed2e8eb4a9471dded383ed9
srilekha2799 <srilekhagondi2007@gmail.com>
2026-05-24 11:35:03 +0530

    testing callback route

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,8 +1,85 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { supabase } from "@/lib/supabase";
+
 export default function AuthCallback() {
 
+  const router = useRouter();
+
+  useEffect(() => {
+
+    const handleAuth =
+      async () => {
+
+        // Wait a little for session
+        await new Promise((resolve) =>
+          setTimeout(resolve, 1500)
+        );
+
+        const {
+          data: { session },
+          error,
+        } =
+          await supabase.auth.getSession();
+
+        console.log(
+          "SESSION:",
+          session
+        );
+
+        console.log(
+          "ERROR:",
+          error
+        );
+
+        if (session) {
+
+          router.replace(
+            "/homepage"
+          );
+
+        } else {
+
+          router.replace(
+            "/login"
+          );
+        }
+      };
+
+    handleAuth();
+
+  }, [router]);
+
   return (
-    <h1>
-      Callback Working
-    </h1>
+
+    <main
+      className="
+      flex
+      min-h-screen
+      items-center
+      justify-center
+      bg-[#EEF4FF]
+
+      dark:bg-[#020817]
+    "
+    >
+
+      <h1
+        className="
+        text-3xl
+        font-bold
+        text-[#5B7CFA]
+      "
+      >
+
+        Signing you in...
+
+      </h1>
+
+    </main>
   );
 }
