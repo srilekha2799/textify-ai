b03597f67e5d9a48ab88e1ba634928276bcab7e3
srilekha2799 <srilekhagondi2007@gmail.com>
2026-05-26 12:31:47 +0530

    test api

--- app/api/simplify/route.ts
+++ app/api/simplify/route.ts
@@ -1,7 +1,73 @@
-export async function GET() {
+export const runtime = "nodejs";
+import { NextResponse } from "next/server";
 
-  return Response.json({
-    message: "API WORKING",
-  });
+export async function POST(req: Request) {
 
+  try {
+
+    const body = await req.json();
+
+    if (!body.text) {
+
+      return NextResponse.json({
+        error: "No text provided",
+      });
+    }
+
+    const response = await fetch(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        method: "POST",
+
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+
+          "Content-Type":
+            "application/json",
+        },
+
+        body: JSON.stringify({
+
+          model: "openrouter/auto",
+
+          messages: [
+            {
+              role: "system",
+
+              content:
+                "You simplify difficult text.",
+            },
+
+            {
+              role: "user",
+
+              content: body.text,
+            },
+          ],
+        }),
+      }
+    );
+
+    const data =
+      await response.json();
+
+    const result =
+      data?.choices?.[0]?.message
+        ?.content;
+
+    return NextResponse.json({
+      result:
+        result ||
+        "No response generated",
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json({
+      error:
+        "Something went wrong",
+    });
+  }
 }
