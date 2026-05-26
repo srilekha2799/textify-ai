0b2ed747bb20e4e93ba91b58fe4b06d32f779378
srilekha2799 <srilekhagondi2007@gmail.com>
2026-05-26 13:33:14 +0530

    eslint fix1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,35 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import {
+  defineConfig,
+  globalIgnores,
+} from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+import nextVitals
+from "eslint-config-next/core-web-vitals";
+
+import nextTs
+from "eslint-config-next/typescript";
+
+const eslintConfig =
+  defineConfig([
+
+    ...nextVitals,
+
+    ...nextTs,
+
+    {
+      rules: {
+        "react-hooks/set-state-in-effect":
+          "off",
+      },
+    },
+
+    // Override default ignores
+    globalIgnores([
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ]),
+  ]);
 
 export default eslintConfig;
-rules: {
-  "react-hooks/set-state-in-effect": "off",
-}
